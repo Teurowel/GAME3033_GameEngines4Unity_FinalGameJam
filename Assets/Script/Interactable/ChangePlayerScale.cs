fe04a48857a6541313f9efd7543766a6af36d18c
Doosung Jang <teurowel@gmail.com>
2021-04-23 00:09:52 +0900

    make palyer small

--- Assets/Script/Interactable/ChangePlayerScale.cs
+++ Assets/Script/Interactable/ChangePlayerScale.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MakePlayerBig : MonoBehaviour, IInteractable
+public class ChangePlayerScale : MonoBehaviour, IInteractable
 {
     [SerializeField] float effectTime = 5;
     [SerializeField] List<Renderer> rendererToDiable;
 
     [SerializeField] float playerMass = 30;
     [SerializeField] float playerMoveForce = 30;
+    [SerializeField] Vector3 playerScale = Vector3.zero;
 
     float originPlayerMoveForce = 0.0f;
     float originPlayerMass = 0.0f;
+    Vector3 originPlayerScale = Vector3.zero;
 
     Player interacter = null;
     Collider _collider;
@@ -25,19 +27,26 @@ public class MakePlayerBig : MonoBehaviour, IInteractable
     {
         Debug.Log("OnInteracted");
         interacter = _interacter;
-        //interacter.SetScale(new Vector3(2, 2, 2));
-        interacter.transform.localScale = new Vector3(2, 2, 2);
+        
+        //Set scale
+        originPlayerScale = interacter.transform.localScale;
+        interacter.transform.localScale = playerScale;
 
+        //Set mass
         originPlayerMass = interacter.GetComponent<Rigidbody>().mass;
         interacter.GetComponent<Rigidbody>().mass = playerMass;
+
+        //Set move force
         originPlayerMoveForce = interacter.GetComponent<PlayerMovement>().moveForce;
         interacter.GetComponent<PlayerMovement>().moveForce = playerMoveForce;
 
+        //Disable renderer
         foreach (Renderer renderer in rendererToDiable)
         {
             renderer.enabled = false;
         }
 
+        //Disable collider
         _collider.enabled = false;
 
         //call disable effect after some time
@@ -46,8 +55,7 @@ public class MakePlayerBig : MonoBehaviour, IInteractable
 
     public void DisableEffect()
     {
-        //interacter.SetScale(new Vector3(1, 1, 1));
-        interacter.transform.localScale = new Vector3(1, 1, 1);
+        interacter.transform.localScale = originPlayerScale;
         interacter.GetComponent<Rigidbody>().mass = originPlayerMass;
         interacter.GetComponent<PlayerMovement>().moveForce = originPlayerMoveForce;
 
